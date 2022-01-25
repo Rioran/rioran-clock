30177c4032b60da122062458eb25aca751883517
Rioran <33312275+Rioran@users.noreply.github.com>
2022-01-25 20:54:38 +0300

    - misplaced bracket

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var time_cache = new Date().toTimeString().slice(0,8);
 
 function render_time() {
     var time = new Date().toTimeString().slice(0,8);
-    if (time !== time_cache)) {
+    if (time !== time_cache) {
       document.getElementById("clock-digits-p").innerHTML = time;
       time_cache = time;
     }
